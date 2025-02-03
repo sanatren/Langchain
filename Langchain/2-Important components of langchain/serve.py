baa8a28eee2dc0c16673531054a1af21dcde7e77
sanatan khemariya <sanatankhemariya@gmail.com>
2025-02-03 23:07:10 +0530

    Fixing unstaged changes

---
+++ Langchain/2-Important components of langchain/serve.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langserve import add_routes
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_KEY")
+
+model = ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+
+
+# 1. Create prompt template
+generic_temp = "Translate the following into {language}:"
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system",generic_temp),("user","{text}")]
+)
+
+parser = StrOutputParser()
+
+##create chain
+chain = prompt|model|parser
+
+##app defination
+app = FastAPI(title="Langchain Server",
+              version = "1.0",
+              description = "API server for using langchain runnable interfaces")
+
+
+## Adding routes
+add_routes(
+
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app,host="localhost",port=8000)
